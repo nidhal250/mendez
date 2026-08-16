90eb0eed1f61f5a65931029fb4e1f5e93f225f28
𝐏𝐫𝟏𝐦𝐞 𝐍𝟏𝐝𝐡@𝐋 ヤ! <nidhaltalbi82@gmail.com>
2026-08-16 17:15:46 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,15 +39,7 @@ intents.members = True
 intents.voice_states = True
 intents.guilds = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
-﻿
-
- 
- 
- 
- 
-
-
+bot = commands.Bot(command_prefix="!", intents=intents)﻿
 # ==========================
 # HELLO COMMAND
 # ==========================
